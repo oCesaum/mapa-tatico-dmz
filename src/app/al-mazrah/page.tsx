77de6dc6f26fa67355228d8b70b5a38a1058991a
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-05-07 12:51:51 -0300

    versão 1, mapa de Al Mazrah

--- src/app/al-mazrah/page.tsx
+++ src/app/al-mazrah/page.tsx
@@ -1,19 +1,36 @@
-import Image from "next/image";
+"use client"
 
-import nordeste from "../../../public/al-mazrah/nordeste.png";
-import sudeste from "../../../public/al-mazrah/sudeste.png";
-import sudoeste from "../../../public/al-mazrah/sudoeste.png";
-import noroeste from "../../../public/al-mazrah/noroeste.png";
+import L from "leaflet";
+import {
+  MapContainer,
+  ImageOverlay,
+} from "react-leaflet";
 
 export default function AlMazrah() {
+  const bounds = [
+    [0, 0],
+    [512, 512],
+  ];
+
   return (
-    <main className="bg-zinc-800 text-zinc-300 min-h-screen grid place-content-center">
-      <div className="grid grid-cols-2">
-        <Image height={512} width={512} src={noroeste} alt="Noroeste de Al Mazrah"/>
-        <Image height={512} width={512} src={nordeste} alt="Nordeste de Al Mazrah"/>
-        <Image height={512} width={512} src={sudoeste} alt="Sudoeste de Al Mazrah"/>
-        <Image height={512} width={512} src={sudeste} alt="Sudeste de Al Mazrah"/>
-      </div>
+    <main>
+      <MapContainer
+        className="w-screen h-screen bg-zinc-800"
+        center={[256, 256]}
+        zoom={0}
+        scrollWheelZoom={true}
+        crs={L.CRS.Simple}
+        attributionControl={false}
+        style={{ backgroundColor: "#000" }}
+      >
+        <ImageOverlay
+          url="/al-mazrah/al-mazrah.jpg"
+          bounds={[
+            [0, 0],
+            [512, 512],
+          ]}
+        />
+      </MapContainer>
     </main>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,26 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Mapa tático DMZ',
-  description: 'Este é um mapa tático de um brasileiro para jogadores de DMZ',
-}
+  title: "Mapa tático DMZ",
+  description: "Este é um mapa tático de um brasileiro para jogadores de DMZ",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+          integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+          crossOrigin=""
+        />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
